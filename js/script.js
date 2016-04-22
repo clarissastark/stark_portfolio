4f3bafe476bb3ae091bbf04e3b1632375a42ac40
Clarissa Stark <clarissa.stark@gmail.com>
2016-04-22 14:38:36 -0400

    modal 'x' now closes popup

--- js/script.js
+++ js/script.js
@@ -25,13 +25,10 @@ thumb3.onclick = function() {
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal1.style.display = "none";
-};
-span.onclick = function() {
     modal2.style.display = "none";
-};
-span.onclick = function() {
     modal3.style.display = "none";
 };
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
